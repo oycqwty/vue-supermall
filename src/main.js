6924c6a8d726b6db2697253afc4a87696d58f2e8
wty <55989150@qq.com>
2020-06-05 15:15:42 +0800

    更新首页导航头

--- src/main.js
+++ src/main.js
@@ -10,3 +10,11 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
+// vue-router>=3.0版本回调形式以及改成promise api的形式了，返回的是一个promise，如果路由地址跳转相同，且没有捕获到错误
+// import Router from 'vue-router'
+// const originalPush = Router.prototype.push
+// Router.prototype.push = function push(location) {
+//  return originalPush.call(this, location).catch(err => err)
+// }
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,25 +1,17 @@
 <template>
   <div id="home">
-    <!-- <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"/>
-    <recommend-view :recommends="recommends"/> -->
-    <h2>首页</h2>
+      <nav-bar class="home-nav">
+        <div slot="center">购物街</div>
+      </nav-bar>
   </div>
 </template>
 
 <script>
-  // import NavBar from 'components/common/navbar/NavBar';
-  // import HomeSwiper from './childComps/HomeSwiper'
-  // import RecommendView from './childComps/RecommendView'
-
-  // import {getHomeMultidata} from "network/home";
-
+import NavBar from '@/components/common/navbar/NavBar' 
   export default {
     name: "Home",
     components: {
-      // NavBar,
-      // HomeSwiper,
-      // RecommendView
+      NavBar
     },
     data() {
       return {
