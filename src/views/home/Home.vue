07115615b2c13cbfb1fdd4fcd942d449fd5ba485
wty <55989150@qq.com>
2020-06-10 20:07:32 +0800

    完成首页数据展示,bscroll滑动,置顶功能

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,47 +1,146 @@
 <template>
   <div id="home">
-      <nav-bar class="home-nav">
-        <div slot="center">购物街</div>
-      </nav-bar>
-      <home-swiper :banners="banners"/>
-      <recommend-view :recommends="recommends"></recommend-view>      
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <!-- 滚动效果 -->
+    <scroll class="content"
+            ref="scroll"
+            :probe-type="3"
+            @scroll="contentScroll"
+            :pull-up-load="true"
+            @pullingUp="loadMore">
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends"></recommend-view>
+    <featrue-view></featrue-view>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']" @tabClick="tabClick"></tab-control>    
+    <goods-list :goods="showGoods"></goods-list> <!-- 首页数据的展示 -->
+    </scroll>
+
+    <back-top @click.native="backClick" v-show="isShowBackTop"></back-top>
+
+    
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar' 
-import HomeSwiper from 'views/home/childComponents/HomeSwiper'
-import RecommendView from '@/views/home/childComponents/RecommendView'
+import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "views/home/childComponents/HomeSwiper";
+import RecommendView from "@/views/home/childComponents/RecommendView";
+import FeatrueView from "@/views/home/childComponents/FeatrueView";
+import TabControl from "components/content/tabControl/tabControl";
+import GoodsList from "components/content/goods/GoodsList";
+import Scroll from 'components/common/scroll/Scroll'
+import BackTop from 'components/content/backTop/BackTop'
 
-import {getHomeMultidata} from "network/home";
+import { getHomeMultidata, getHomeGoods } from "network/home";
 
-  export default {
-    name: "Home",
-    components: {
-      NavBar,
-      HomeSwiper,
-      RecommendView
-    },
-    data() {
-      return {
-        banners: [],
-        recommends: []
-      }
-    },
-    created() {
-      // 1.请求多个数据
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+    FeatrueView,
+    TabControl,
+    GoodsList,
+    Scroll,
+    BackTop
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      },
+      currentType: "pop",//默认显示流行数据
+      isShowBackTop:false,//默认置顶按钮不显示
+    };
+  },
+  created() {
+    this.getHomeMultidata();
+    this.getHomeGoods("pop");
+  },
+  methods: {
+    // 1.请求多个数据
+    getHomeMultidata() {
       getHomeMultidata().then(res => {
-        // this.result = res;
         this.banners = res.data.banner.list;
         this.recommends = res.data.recommend.list;
-      })
+      });
+    },
+    loadMore() {
+      this.getHomeGoods(this.currentType);
+      this.$refs.scroll.scroll.refresh()
+    },
+    //请求商品数据
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then(res => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+        this.$refs.scroll.scroll.finishPullUp()  //到底 上拉加载数据
+      });
+    },
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+     contentScroll(position) {
+        this.isShowBackTop = (-position.y) > 1000
+      },
+      backClick(){
+        this.$refs.scroll.scroll.scrollTo(0,0,500);//延时500ms
+      }
+  },
+  computed: {
+    showGoods() {
+      console.log(this.currentType);
+      return this.goods[this.currentType].list;
     }
   }
+};
 </script>
 
 <style scoped>
-  .home-nav {
-    background-color: var(--color-tint);
-    color: #fff;
-  }
+#home {
+  padding-top: 44px;
+  height: 100vh;
+  position: relative;
+}
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
+  z-index: 9;
+}
+.content {
+  overflow: hidden;
+
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
 </style>
